597eed35c6b3072e3234c33e744535f4ed08a817
Marco Nassabain <marco.nassabain@hotmail.com>
2021-05-16 17:00:31 +0200

    ✨ Add shareUrl to router

--- index.js
+++ index.js
@@ -31,6 +31,29 @@ let capitalize = (s) => {
 let rawData = fs.readFileSync('phrases.json');
 let phrases = JSON.parse(rawData);
 
+// Share URL
+app.get('/share/:messageId/:name?', (req, res) => {
+  const messageId = req.params.messageId;
+  const message = phrases[messageId];
+
+  let name = req.params.name;
+  if (!name) {
+    name = 'Mon amour,';
+  } else {
+    name = `${capitalize(name)},`;
+  }
+
+  // Share url = current url
+  const shareUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  
+  // render page with phrase
+  res.render('letter', {
+    title: name,
+    message,
+    shareUrl, 
+  });
+})
+
 // base route
 app.get('/:name?', (req, res) => {
   const baseUrl = req.protocol + '://' + req.get('host');
@@ -46,8 +69,9 @@ app.get('/:name?', (req, res) => {
     name = `${capitalize(name)},`;
   }
 
-  // Calculate shareUrl of page
-  const shareUrl = `${baseUrl}/share/${messageId}/${req.params.name}`;
+  // Get shareUrl of page
+  const shareName = req.params.name ? req.params.name : '';
+  const shareUrl = `${baseUrl}/share/${messageId}/${shareName}`;
 
   // render page with phrase
   res.render('letter', {
